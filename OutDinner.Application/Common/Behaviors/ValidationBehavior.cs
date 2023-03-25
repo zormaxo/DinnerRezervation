24bf17e0c604eb7677137d35978fe0bd8bd1e972
omer <okocadere@gmail.com>
2023-03-25 15:42:45 +0300

    fluent validation added

---
+++ OutDinner.Application/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,32 @@
+﻿using ErrorOr;
+using FluentValidation;
+using MediatR;
+using OutDinner.Application.Authentication.Commands.Register;
+using OutDinner.Application.Authentication.Common;
+
+namespace OutDinner.Application.Common.Behaviors;
+
+public class ValidateRegisterCommandBehavior : IPipelineBehavior<RegisterCommand, ErrorOr<AuthenticationResult>>
+{
+    private readonly IValidator<RegisterCommand> _validator;
+
+    public ValidateRegisterCommandBehavior(IValidator<RegisterCommand> validator) { _validator = validator; }
+
+    public async Task<ErrorOr<AuthenticationResult>> Handle(
+        RegisterCommand request,
+        RequestHandlerDelegate<ErrorOr<AuthenticationResult>> next,
+        CancellationToken cancellationToken)
+    {
+        var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+
+        if (validationResult.IsValid)
+        {
+            return await next();
+        }
+
+        var errors = validationResult.Errors.ConvertAll(
+            validationFailure => Error.Validation(validationFailure.PropertyName, validationFailure.ErrorMessage));
+
+        return errors;
+    }
+}
--- OutDinner.Application/DependencyInjection.cs
+++ OutDinner.Application/DependencyInjection.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using ErrorOr;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using OutDinner.Application.Authentication.Commands.Register;
+using OutDinner.Application.Authentication.Common;
+using OutDinner.Application.Common.Behaviors;
+using System.Reflection;
 
 namespace OutDinner.Application;
 
@@ -7,6 +14,10 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+        //services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidateRegisterCommandBehavior));
+        services.AddScoped<IPipelineBehavior<RegisterCommand, ErrorOr<AuthenticationResult>>, ValidateRegisterCommandBehavior>();
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
         return services;
     }
 }
