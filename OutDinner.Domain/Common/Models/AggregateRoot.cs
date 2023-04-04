1b5f6f25e750a55dfddd9f384e7f36bc334f266d
omer <okocadere@gmail.com>
2023-04-04 12:26:35 +0300

    models added

---
+++ OutDinner.Domain/Common/Models/AggregateRoot.cs
@@ -0,0 +1,14 @@
+namespace BuberDinner.Domain.Common.Models;
+
+public abstract class AggregateRoot<TId> : Entity<TId>
+    where TId : notnull
+{
+    protected AggregateRoot(TId id) : base(id)
+    {
+    }
+
+    protected AggregateRoot()
+        : base()
+    {
+    }
+}
